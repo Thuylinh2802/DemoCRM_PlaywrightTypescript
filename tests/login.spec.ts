fb055698f1e84d9c13ad2bc4e3930649ecd84fbe
Thuylinh2802 <20020112@vnu.edu.vn>
2026-07-14 22:43:22 +0700

    refactor(test): implement data-driven pattern for login tests

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,51 +1,23 @@
 import {test} from "@playwright/test"
 import { LoginPage } from "../src/pages/login.page"
+import { loginData } from "../src/data/login.data"
 
-test("Login success", async ({page}) => {
-    const loginPage = new LoginPage(page)
 
-    await loginPage.goto("https://crm.anhtester.com/admin/authentication")
-    await loginPage.login("admin@example.com", "123456")
-    await loginPage.verifyLoginSuccess()
+test.describe("Login Test", () => {
+    loginData.forEach(({name, email, password, expected}) => {
+        test(name, async ({page}) => {
+            const loginPage = new LoginPage(page)
+
+            await loginPage.goto("/admin/authentication")
+            await loginPage.login(email, password)
+
+            if(expected === "success") {
+                await loginPage.verifyLoginSuccess()
+            } else {
+                await loginPage.verifyLoginFail()
+            }
+        })
+    })
+
 })
-
-test("Login fail with empty email", async ({page}) => {
-    const loginPage = new LoginPage(page)
-
-    await loginPage.goto("https://crm.anhtester.com/admin/authentication")
-    await loginPage.login("", "123456")
-    await loginPage.verifyLoginFail()
-})
-
-// test.describe("Login Test", () => {
-
-//     loginData.forEach(({name, email, password, expected}) => {
-
-//         test(name, async ({page}) => {
-
-//             const loginPage = new LoginPage(page)
-
-            
-
-//             await loginPage.goto("/admin/authentication")
-
-//             await loginPage.login(email, password)
-
-
-
-//             if(expected === "success") {
-
-//                 await loginPage.verifyLoginSuccess()
-
-//             } else {
-
-//                 await loginPage.verifyLoginFail()
-
-//             }
-
-//         })
-
-//     })
-
-// })
 
